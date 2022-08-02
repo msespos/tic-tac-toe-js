218e836ea2fecbe4c7b86d81b4394e8fa53959fd
Mike Esposito <mikeesposito6852@gmail.com>
2022-08-02 14:46:06 -0700

    Add board and game controllers and player factory

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-alert('hello');
+const boardController = (() => {
+  let gameBoard = [
+    ["X", "O", "X"],
+    ["O", "X", "O"],
+    ["X", "O", "X"]
+  ];
+  return { gameBoard };
+})();
+
+const gameController = (() => {
+})();
+
+const Player = (name) => {
+  return { name };
+};
